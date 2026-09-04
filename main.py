5f672ad596b3c2bfa40a117fcba82768d8066a84
koreinne <reyes.kf@obmontessori.edu.ph>
2026-09-04 11:51:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#Reyes, Korinne
+studname = "Yujin Kim"
+studage = 15
+studheight1 = 
+countries_visit = []
+student_type = 
+stud_pref{
+    "color":
+    "Car brand":
+    "shoe size":
+    "bff": 
+}
+stud_fruits= {
+
+}
+days_week = {
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+}
+print("Name:", studname)
+print("Age:", studage)
